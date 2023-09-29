3a60c1e4fa87986f964378bea2ee85896d4f8f3a
Anton Shchukin <a.a.shchukin@gmail.com>
2023-09-29 22:43:19 +0700

    profile

--- src/scripts/_profile.js
+++ src/scripts/_profile.js
@@ -1,28 +1,28 @@
 (function($) {
 
-    const $profile = $('.profile');
+    const $html = $('html');
 
     $('.action--profile').on('click', function (event) {
-        $profile.addClass('profile--expanded');
+        $html.addClass('profile-expanded');
     });
 
 
 
     $('.profile__close').on('click', function (event) {
         event.preventDefault();
-        $profile.removeClass('profile--expanded');
+        $html.removeClass('profile-expanded');
     });
 
     $(document).on('keyup', function (event) {
         if (event.keyCode === 27) {
-            $profile.removeClass('profile--expanded');
+            $html.removeClass('profile-expanded');
         }
     });
 
     /* Клик вне .profile */
     $(document).on('click', function (event) {
         if (!$(event.target).closest('.profile, .action--profile').length) {
-            $profile.removeClass('profile--expanded');
+            $html.removeClass('profile-expanded');
         }
     });
 
