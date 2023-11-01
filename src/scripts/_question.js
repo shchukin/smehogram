640c0639ce8f36c4a0801eb0a02f9cfba0392987
Anton Shchukin <a.a.shchukin@gmail.com>
2023-11-02 03:51:59 +0700

    round

---
+++ src/scripts/_question.js
@@ -0,0 +1,20 @@
+(function ($) {
+
+    let isDesktop = window.matchMedia("(width < 740px)").matches;
+
+    $('.question__control').on('click', function () {
+        let toScroll;
+        if ($(this).hasClass('question__control--next')) {
+            toScroll = isDesktop ? $(window).outerWidth(): '900px';
+        } else {
+            toScroll = 0;
+        }
+        $('.question__swipe').addClass('question__swipe--disable-snapping');
+        $('.question__swipe').animate({
+            scrollLeft: toScroll
+        }, 400, function () {
+            $('.question__swipe').removeClass('question__swipe--disable-snapping');
+        });
+    });
+
+})(jQuery);
