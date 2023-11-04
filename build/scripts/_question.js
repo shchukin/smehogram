4a62510b0f9ed31d61caf41a9ed06c44e6ab9b1e
Anton Shchukin <a.a.shchukin@gmail.com>
2023-11-04 13:20:24 +0700

    build

--- build/scripts/_question.js
+++ build/scripts/_question.js
@@ -5,8 +5,9 @@
     $('.question__control').on('click', function () {
         let toScroll;
         if ($(this).hasClass('question__control--next')) {
-            toScroll = isDesktop ? $(window).outerWidth(): '900px';
-        } else {
+            toScroll = isDesktop ? $(window).outerWidth() : '900px';
+        }
+        if ($(this).hasClass('question__control--prev')) {
             toScroll = 0;
         }
         $('.question__swipe').addClass('question__swipe--disable-snapping');
